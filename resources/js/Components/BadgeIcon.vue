eb9b9aed2ad2788d0a4cbdb4931e41a073c6abe2
Xander <xander@xadi.nl>
2025-12-16 12:48:26 +0100

    fix: remove borders

--- resources/js/Components/BadgeIcon.vue
+++ resources/js/Components/BadgeIcon.vue
@@ -18,36 +18,12 @@ const sizeClasses = computed(() => {
         default: return 'w-12 h-12';
     }
 });
-
-const tierClasses = computed(() => {
-    if (props.isShame) {
-        return 'ring-red-400 dark:ring-red-600';
-    }
-
-    switch (props.tier) {
-        case 'platinum': return 'ring-purple-300 dark:ring-purple-400';
-        case 'gold': return 'ring-yellow-400 dark:ring-yellow-500';
-        case 'silver': return 'ring-neutral-300 dark:ring-neutral-400';
-        case 'bronze': return 'ring-orange-400 dark:ring-orange-500';
-        default: return 'ring-neutral-200 dark:ring-neutral-600';
-    }
-});
-
-const tierGlow = computed(() => {
-    if (props.isShame) return '';
-
-    switch (props.tier) {
-        case 'platinum': return 'shadow-purple-300/50 dark:shadow-purple-500/30';
-        case 'gold': return 'shadow-yellow-400/50 dark:shadow-yellow-500/30';
-        default: return '';
-    }
-});
 </script>
 
 <template>
     <div
-        class="relative inline-block rounded-full ring-2 overflow-hidden bg-neutral-100 dark:bg-neutral-800"
-        :class="[sizeClasses, tierClasses, tierGlow && 'shadow-lg ' + tierGlow]"
+        class="relative inline-block rounded-full overflow-hidden bg-neutral-100 dark:bg-neutral-800"
+        :class="sizeClasses"
         :title="name"
     >
         <img
